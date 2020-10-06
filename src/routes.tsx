a04524a7fae6b29da7efc4dc804cf0bd0d1bb3bb
loxt <emannuelloxt@hotmail.com>
2020-10-06 14:11:49 -0400

    chore: WIP home page

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,10 +7,14 @@ import Home from './pages/Home';
 
 const Stack = createStackNavigator();
 
+const screenOptions = {
+  headerShown: false
+};
+
 const Routes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={screenOptions}>
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
