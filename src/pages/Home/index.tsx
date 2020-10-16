f2e7a81ee958557ed30672fa2e07c10049c652b0
loxt <emannuelloxt@hotmail.com>
2020-10-15 23:17:02 -0400

    feat: add home styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-import {SafeAreaView} from "./styles";
-import {StatusBar} from "expo-status-bar";
-import {Text, Alert, ActivityIndicator} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Text, Alert, ActivityIndicator } from "react-native";
+import { SafeAreaView } from "./styles";
 
 export default function Home() {
   const [banner, setBanner] = useState([]);
@@ -13,7 +13,9 @@ export default function Home() {
   useEffect(() => {
     async function loadData() {
       try {
-        const response = await fetch("http://my-json-server.typicode.com/pablohdev/app-ifood-clone/db");
+        const response = await fetch(
+          "http://my-json-server.typicode.com/pablohdev/app-ifood-clone/db"
+        );
 
         const data = await response.json();
 
@@ -30,18 +32,22 @@ export default function Home() {
     loadData();
   }, []);
 
+  const ViewHome = (props: any) => {
+    return <Text>Principal</Text>;
+  };
+
   if (!loaded)
     return (
       <SafeAreaView>
-        <Text>Carregando...</Text>
+        <ActivityIndicator color="#ea1d2c" size="large" />
       </SafeAreaView>
-    )
+    );
 
   return (
     <>
-      <StatusBar style='dark' />
+      <StatusBar style="dark" />
       <SafeAreaView>
-        <Text>Home</Text>
+        <ViewHome />
       </SafeAreaView>
     </>
   );
